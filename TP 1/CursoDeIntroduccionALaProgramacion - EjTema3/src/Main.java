f5488f3385e454145777146dbd4ccd66caf6b79e
NicoManfredi <77796768+NicoManfredi@users.noreply.github.com>
2022-09-09 17:45:12 -0300

    Add files via upload

---
+++ TP 1/CursoDeIntroduccionALaProgramacion - EjTema3/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    //PRIMERA PARTE:
+    public static void main(String[] args){
+        suma(10,50,30);
+    }
+    public static void suma(int a, int b, int c){
+        System.out.println(a + b +c);
+    }
+
+
+/*    //SEGUNDA PARTE:
+    public static void main(String[] args){
+        Coche miCoche = new Coche();
+        miCoche.agregarPuerta();
+
+        System.out.println(miCoche.puertas);
+    }
+    static class Coche{
+        public int puertas = 4;
+        public void agregarPuerta(){
+            this.puertas++;
+        }
+    }*/
+}
+
+
